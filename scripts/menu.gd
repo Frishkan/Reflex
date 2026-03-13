7c96e3e407e7ee9ecefa1fa2858c81155fcb9e7f
GTX950 <ralfsbz@gmail.com>
2026-03-13 16:34:24 +0200

    Added more buttons to pause menu and fixed some button backgrounds showing

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,3 +5,7 @@ func _on_return_pressed() -> void:
 	self.hide()
 	$/root/game/hud.show()
 	$/root/game/FightScene.show()
+	
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
